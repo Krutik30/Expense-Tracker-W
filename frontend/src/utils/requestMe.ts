ad339a3063d63b88348a97faa2494e62df89b786
Krutik30 <agherakrutik99@gmail.com>
2024-02-23 18:43:44 +0530

    requestMe : function

--- frontend/src/utils/requestMe.ts
+++ frontend/src/utils/requestMe.ts
@@ -2,13 +2,19 @@ import { HOST_URL } from '../../config'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const requestMe = async(route: string, option: any) => {
+
+    option = {
+        ...option,
+        'Content-Type': 'application/json',
+    }
+
     return await fetch(HOST_URL+ route, option)
         .then(async (res)=>{
             if (!res || !res.ok || res.status >= 400) {
                 throw res;
             }
             const response = await res.json();
-            // console.log({response});
+            
             if (response.status !== 'success') {
                 throw response;
             } else {
