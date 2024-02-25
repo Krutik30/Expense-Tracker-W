418a9719ea552a78965845fdc6b45441b7e229ef
Krutik30 <agherakrutik99@gmail.com>
2024-02-25 16:37:47 +0530

    Routing fix

--- frontend/src/components/ProtectedRoutes.tsx
+++ frontend/src/components/ProtectedRoutes.tsx
@@ -5,7 +5,7 @@ import { Page404 } from '../pages/Page404';
 
 interface ProtectedRouteProps {
     children: React.ReactElement;
-    roleRequire: 'ADMIN' | 'EMPLOYEE' | false;
+    roleRequire: ('ADMIN' | 'EMPLOYEE' | false)[];
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
@@ -14,7 +14,7 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
 }) => {
     const { pathname } = useLocation();
     const openRoutes = ['/auth/login', '/auth/signup'];
-    let userLogged = {
+    let userLogged: any = {
         auth: false,
         role: Role.emp 
     };
@@ -29,7 +29,7 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
     return userLogged.auth || openRoutes.includes(pathname)
         ? (pathname === '/auth/login') && userLogged.auth
             ? <Navigate to='/dashboard' replace />
-            : (roleRequire === false || roleRequire === userLogged?.role)
+            : (roleRequire.includes(false) || roleRequire.includes(userLogged?.role))
                 ? children
                 : <Page404 />
         : <Navigate to='/auth/login' replace />;
