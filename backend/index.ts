fc95b4c01f01934807a6dc9019212dd8e29d77e1
Krutik30 <agherakrutik99@gmail.com>
2024-02-29 18:48:13 +0530

    Cors error

--- backend/index.ts
+++ backend/index.ts
@@ -18,6 +18,14 @@ var corsOptions = {
     credentials: true
 };
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://expence-tracker-hackdspring.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 // app.use(express.json());
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({
