f96e1c2bda9345042309b36ca080c50c41ce62f5
Krutik30 <agherakrutik99@gmail.com>
2024-02-29 18:42:39 +0530

    Cors error

--- backend/index.ts
+++ backend/index.ts
@@ -12,7 +12,7 @@ import advanceRoute from './routes/advance';
 const app = express();
 
 var corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://expence-tracker-hackdspring.vercel.app/'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://expence-tracker-hackdspring.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'authorization'],
     credentials: true
